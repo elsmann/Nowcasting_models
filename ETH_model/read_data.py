737e9f11456a6eeff2f4dab41b8c474caed5a530
elsmann <38163625+elsmann@users.noreply.github.com>
2023-01-23 15:13:53 +0100

    cleaned code

--- ETH_model/read_data.py
+++ ETH_model/read_data.py
@@ -20,7 +20,6 @@ feature_description = {
 def _parse_function_radar(example_proto):
     example = tf.io.parse_single_example(example_proto, feature_description)
     example['image_radar'] = tf.io.parse_tensor(example['image_radar'], tf.float32, name=None)
-    example['image_radar'] = min_max_normalize(example['image_radar'], 0, 1024)
     example.pop('image_eth')
     example.pop('exists_both')
     return example
@@ -28,9 +27,7 @@ def _parse_function_radar(example_proto):
 def _parse_function_ETH(example_proto):
     example = tf.io.parse_single_example(example_proto, feature_description)
     example['image_radar'] = tf.io.parse_tensor(example['image_radar'], tf.float32, name=None)
-    example['image_radar'] = min_max_normalize(example['image_radar'], 0, 1024)
     example['image_eth'] = tf.io.parse_tensor(example['image_eth'], tf.float32, name=None)
-    example['image_eth'] = min_max_normalize(example['image_eth'], 0.2, 16)
 
     return example
 
@@ -47,29 +44,6 @@ def only_image_ETH(image_dataset):
 
 def check_dates(image_dataset):
     return zip(image_dataset['date'], image_dataset['date'])
-
-
-def filter_windows_old(d, expected_len, ISS=True, ISS_value=200):
-    window_ok = True
-    if len(d['date']) != expected_len:
-        window_ok = False
-    for example in d['exists_radar']:
-        # the second column is whether the image is ok
-        if example == 0:
-            window_ok = False
-            break
-    # Importance Sampling Scheme
-    # stochastically filter out sequences that contain little rainfall
-    if ISS:
-        rain_sum = 0.0
-        for element in d['image_sum_radar']:
-            rain_sum += element
-        prob = 1 - tf.math.exp(-(rain_sum / ((ISS_value* 12)/1000)))
-        print(prob)
-        prob = tf.math.minimum(1.0, prob + 0.1)
-        if prob < tf.random.uniform(shape=[]):
-            window_ok = False
-    return window_ok
 
 
 def filter_windows(d, expected_len = 22, ETH = False, ISS=True, ISS_value = 200, base_prob = 0.3):
@@ -133,15 +107,7 @@ def read_TFR(path, ETH= False, batch_size=12, window_shift=1, ISS = True, ISS_va
         else:
             dataset = dataset.map(only_image_radar, num_parallel_calls=tf.data.AUTOTUNE)
     dataset = dataset.flat_map(lambda window: window.batch(window_size))
-    #dataset = dataset.shuffle(buffer_size=32)  # TODO chnage
+    dataset = dataset.shuffle(buffer_size=1000) 
     dataset = dataset.batch(batch_size)
     dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
     return dataset
-
-
-
-
-x = read_TFR("/Users/frederikesmac/MA/Data/TFR_ETH", check= True, ISS_value= 201, batch_size=2)
-
-for i in x:
-    print(i)
